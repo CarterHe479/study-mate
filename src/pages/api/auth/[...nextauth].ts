df13693b9e5aa91b79a1c9e55153362d286636b4
Carter He <carterhes479@gmail.com>
2025-04-30 17:14:02 -0400

    Initial commit: set up StudyMate project structure

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
