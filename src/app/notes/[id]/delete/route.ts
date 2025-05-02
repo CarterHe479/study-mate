ce30c11b966736630bfcddbdcd38bd8505f694dc
Carter He <carterhes479@gmail.com>
2025-05-02 15:33:30 -0400

    RouteHandlerContext

--- src/app/notes/[id]/delete/route.ts
+++ src/app/notes/[id]/delete/route.ts
@@ -1,16 +1,16 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse, type RouteHandlerContext } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-export async function POST(req: NextRequest, context: { params: { id: string } }) {
+export async function POST(req: NextRequest, context: RouteHandlerContext) {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
     return NextResponse.redirect("/api/auth/signin");
   }
 
-  const { id } = context.params;  // ✅ 注意这里不需要 await 了
+  const { id } = context.params;
 
   await prisma.note.delete({
     where: {
